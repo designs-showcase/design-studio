986bbe9770956fe0dd06fe129d23bd563c4459fa
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-03-10 15:53:30 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar__brand'>DESIGN STUDIO</div>
+        <RxHamburgerMenu className='navbar__icon' />
+      </nav>
+      <style></style>
+    </>
+  );
+}
